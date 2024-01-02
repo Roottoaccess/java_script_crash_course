3afb644a319f0f42f05ee1d61934bbf1f28dad15
NetworkCrab <network123crab@gmail.com>
2024-01-02 13:29:46 +0530

    This is the second practise set

---
+++ practise2.js
@@ -0,0 +1,11 @@
+// create a const object called "profile" to store information shown in the picture.
+const profile = {
+    user_name: "shradhakhapra",
+    is_follow: true,
+    followers: 569000,
+    following: 4,
+    posts: 195,
+    bio: "Apna Collage | Ex-Microsoft, DRDO",
+};
+
+console.log(profile);
