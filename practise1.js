0b03b8b62c6d461911e61034514daa412644f995
NetworkCrab <network123crab@gmail.com>
2024-01-02 13:21:11 +0530

    Practise part 1

---
+++ practise1.js
@@ -0,0 +1,13 @@
+// Create a const object called "product" to 
+// store information shown in picture.
+
+const product = {
+    product_name: "Parker Jotter Standard CT Ball Pen(Black)",
+    product_rating: 4,
+    product_offer: 5,
+    product_price: 270,
+};
+
+console.log(product);
+
+console.log(product.product_price);
