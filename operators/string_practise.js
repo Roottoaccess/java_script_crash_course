03b7ba7790cec7e3aced99f0356ccc69a8291a4a
NetworkCrab <network123crab@gmail.com>
2024-01-03 22:51:13 +0530

    This is the string practise set

---
+++ operators/string_practise.js
@@ -0,0 +1,9 @@
+// Practise set of string
+// Generating a user name with starting value @
+// and ending value the length of the string
+
+let userName = prompt("Enter your name: ");
+
+let update_name = "@"+userName+userName.length;
+
+console.log(update_name);
