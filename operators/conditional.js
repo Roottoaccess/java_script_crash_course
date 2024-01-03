3fa4cf5b7ee4c77deceea04f14e1d899c8294541
NetworkCrab <network123crab@gmail.com>
2024-01-03 12:45:04 +0530

    this is the statement to check the condition and use if else and ternary operator

---
+++ operators/conditional.js
@@ -0,0 +1,41 @@
+// conditional statement
+// Programming a voting machine
+let age = 25;
+
+// if(age >= 18){
+//     console.log("can vote !");
+// } else {
+//     console.log("can not vote !");
+// }
+
+// Mode converter
+let mode = "dark";
+let color;
+
+if(mode === "dark")
+{
+    color = "black";
+} else if(mode === "light") {
+    color = "white";
+}
+// Printing the color variable
+console.log(color);
+
+// checking the given number is odd or even
+let num = 8;
+
+// writing the conditional statement
+if(num % 2 === 0){
+    console.log("Even number !");
+} else {
+    console.log("Odd number !");
+}
+
+// Ternary Operators
+let result = age > 18 ? "adult" : "not adult !";
+console.log(result);
+// Compct if-else
+
+
+// MDN DOCS
+//Explore ........
